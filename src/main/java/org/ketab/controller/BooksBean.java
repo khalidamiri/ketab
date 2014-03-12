7c9c7195dc4bcafaa5203988f38681b87f352d87
Khalid Amiri <khalid.ameery@gmail.com>
2014-03-12 17:48:33 -0400

    Download JS libraries using Bower, and removed the older manually downloaded libraries.

--- src/main/java/org/ketab/controller/BooksBean.java
+++ src/main/java/org/ketab/controller/BooksBean.java
@@ -1,33 +1,59 @@
 package org.ketab.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import javax.ejb.EJB;
 import javax.enterprise.inject.Produces;
 import javax.inject.Named;
 
+import org.apache.commons.io.IOUtils;
 import org.ketab.book.Book;
+import org.ketab.book.BookManager;
 import org.ketab.book.BookManagerBean;
 
-@Named("Books")
+@Named("books")
 public class BooksBean {
 
 	@EJB
-	private BookManagerBean bmb;
+	private BookManager bm;
+	Book newBook;
 
 	public List ListBooks(String sortBy, String sortType) {
-		return bmb.listBooks(sortBy, sortType);
+		return bm.listBooks(sortBy, sortType);
 	}
 	
-	@Produces
-	@Named
-	public boolean addBook(Book book){
-		Book bookToAdd = new Book();
-//		book
-		return false;
+	public boolean addBook(){
+		Book newBook = new Book();
+		newBook.setUploadDate(new Date());
+		//newBook.setPagesCount(pagesCount); //TODO: Extract the number of pages from pdf.
+		//newBook.setAuthors(authors); //TODO: Get Authors using the given ids.
+		//newBook.setUploader(uploader); //TODO: Get the userid from user's session.
+		//newBook.setPublisher(publisher); //TODO: Get publisher object using the given id.
+		try{
+			bm.addBook(newBook);
+		}
+		catch(Exception e){
+			System.out.println(e.getStackTrace());
+			return false;
+		}
+		return true;
 	}
-	public boolean removeBook(long bookId){return false;}
-	public boolean updateBook(Book book){return false;}
-	public boolean viewBook(long bookId){return false;}
+	public boolean removeBook(long bookId){
+		bm.delBook(bookId);
+		return false;	//TODO: Implement this in BookManagerBean
+	}
+	public boolean updateBook(Book book){
+		bm.updateBook(book);
+		return false;	//TODO: Implement this in BookManagerBean
+	}
+	
+	public Book getBook(long bookId){
+		return bm.getBook(bookId);
+	}
 
 }
